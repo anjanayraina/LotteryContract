f65634e8d0d24cdf7d2fe4dcd523b34f5308982e
anjanay <anjanayraina326@gmail.com>
2023-05-30 10:09:09 +0530

    Interface created

--- LotteryConytract.sol
+++ LotteryConytract.sol
@@ -1,9 +1,17 @@
+
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 interface ITokenAddress{
-    function transferFrom(address to , uint amount) external ;
-    function transfer(address to , uint amount) external;
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory );
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 
 }
 contract LotteryContract is AccessControl{
